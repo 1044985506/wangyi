37a42ab082daf88bac00eb7e7eedb6844d20743b
wangning <1044985506@qq.com>
2020-03-11 14:27:31 +0800

    no message

---
+++ t1-wangyi/src/test/java/com/example/wangyi/WangyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wangyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WangyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
